6461e1d98e5f35e101a0f8998944652e5e12de0a
Tobias <neumsnn.tobias@gmail.com>
2025-10-27 14:12:05 +0100

    Update

--- src/components/DarkMode/Darkmode.jsx
+++ src/components/DarkMode/Darkmode.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import "./Darkmode.sass";
+import "../DarkMode/Darkmode.sass";
 
 export default function ThemeToggle() {
   const [isDarkMode, setIsDarkMode] = useState(() => {
